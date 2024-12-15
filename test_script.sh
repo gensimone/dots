99d63e94888da85625932234e319df339d29ff61
gensimone <gensimone.git@gmail.com>
2024-12-15 13:27:49 +0100

    Update

--- test_script.sh
+++
@@ -1,5 +0,0 @@
-eval "$(awk 'NR==1{print "wallpaper=\"" $0 "\""} NR==2{print "fastfetch_image=\"" $0 "\""} NR==3{print "color_name=\"" $0 "\""}' "$1")"
-
-echo $wallpaper
-echo $fastfetch_image
-echo $color_name
