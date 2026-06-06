ab2f586d9c25d332a0619368902494906062ff31
Simone Gentili <gensimone.git@gmail.com>
2026-06-06 08:20:47 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -47,55 +47,39 @@ vim.pack.add({
 })
 
 require("modus-themes").setup({
-	-- Theme comes in two styles `modus_operandi` and `modus_vivendi`
-	-- `auto` will automatically set style based on background set with vim.o.background
 	style = "auto",
 
 	variants = {
-		modus_operandi = "default", -- Set variant for `modus_operandi` style
-		modus_vivendi = "default", -- Set variant for `modus_vivendi` style
+		modus_operandi = "default",
+		modus_vivendi = "default",
 	},
 
-	transparent = true, -- Transparent background (as supported by the terminal)
-	dim_inactive = false, -- "non-current" windows are dimmed
-	hide_inactive_statusline = true, -- Hide statuslines on inactive windows. Works with the standard **StatusLine**, **LuaLine** and **mini.statusline**
-	line_nr_column_background = true, -- Distinct background colors in line number column. `false` will disable background color and fallback to Normal background
-	sign_column_background = true, -- Distinct background colors in sign column. `false` will disable background color and fallback to Normal background
+	transparent = true,
+	dim_inactive = false,
+	hide_inactive_statusline = true,
+	line_nr_column_background = true,
+	sign_column_background = true,
 	styles = {
-		-- Style to be applied to different syntax groups
-		-- Value is any valid attr-list value for `:help nvim_set_hl`
 		comments = { italic = true },
 		keywords = { italic = true },
 		functions = {},
 		variables = {},
 	},
 
-	--- You can override specific color groups to use other groups or a hex color
-	--- Function will be called with a ColorScheme table
-	--- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the ColorScheme table
-	---@param colors ColorScheme
 	on_colors = function(colors) end,
 
-	--- You can override specific highlights to use other groups or a hex color
-	--- Function will be called with a Highlights and ColorScheme table
-	--- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the Highlights and ColorScheme table
-	---@param highlights Highlights
-	---@param colors ColorScheme
 	on_highlights = function(highlights, colors) end,
 })
 
 local cmp = require("cmp")
 require("cmp").setup({
-    window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered()
-    },
+    window = {},
     mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({{ name = "nvim_lsp" }, { name = "path" }})
 })
@@ -141,7 +125,7 @@ require("oil").setup({
     },
 })
 
-vim.g.compile_mode = {} -- Required by compile-mode.nvim
+vim.g.compile_mode = {}
 require("hop").setup({})
 require("multiple-cursors").setup({})
 require("neogit").setup({})
