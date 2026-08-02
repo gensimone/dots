07ee1ab8dd0957b398ecf61c09385ab0936c5b88
Simone Gentili <gensimone.git@gmail.com>
2026-08-02 22:05:42 +0200

    Use toggleterm 'float' instead of 'tab'.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -102,7 +102,7 @@ require("toggleterm").setup({
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
   persist_size = true,
   persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-  direction = 'tab', -- 'vertical' | 'horizontal' | 'tab' | 'float'
+  direction = 'float', -- 'vertical' | 'horizontal' | 'tab' | 'float'
   close_on_exit = true, -- close the terminal window when the process exits
   clear_env = false, -- use only environmental variables from `env`, passed to jobstart()
 
